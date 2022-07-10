c960bb09017e11fe4bc2a3c3ff8c488bb0b58757
potato <89509337+potatofr@users.noreply.github.com>
2022-07-09 18:10:09 -0700

    hi

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Select anchor element
-let more = document.querySelector('.more');
-//Return array
-let audioArr = document.getElementsByTagName('audio');
-console.log(audioArr);
-// Add event listeners
-more.addEventListener('mouseenter', ()=>{
-    audioArr[0].play()
-});
